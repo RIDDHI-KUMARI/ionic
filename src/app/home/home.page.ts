8170fc4675f512122c1b03a3e39b9f19ab9af037
riddhi kumari <riddhilal14vdb@gmail.com>
2021-07-07 20:57:01 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Platform} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  arr:any[]=[];
+  arr1:any[]=[];
 
-  constructor() {}
-
+  constructor(private platform:Platform) {
+    this.platform.ready().then(()=>{
+    for(var i =0;i<100;i++)
+    {
+      var obj = {name:"name"+i,
+    mobilenumber:"mobilenumber"+i};
+    this.arr.push(obj);
+    }
+    this.arr1 = this.arr;
+  })
+  }
+  filterArray(ev:any){
+    this.arr = this.arr1;
+    const val =ev.target.value;
+    if(val && val.trim() != "")
+    {
+      this.arr = this.arr1.filter((item)=>{
+        return (item.name.toLowerCase().indexof(val.toLowerCase()) > -1 || item.mobilenumber.toLowerCase().indexof(val.toLowerCase()) > -1 )
+      })
+    }
+  }
 }
